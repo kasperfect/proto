fe49aa9e673f5d528ed35fc69d1aa89ac3de48cc
tckaland <thomas@kaland.as>
2017-05-07 09:44:43 +0200

    Add files via upload

--- Circuit.py
+++ Circuit.py
@@ -1,28 +1,30 @@
 __author__ = 'Thomas Kaland'
-# _*_ coding: utf-8 _*_
+# -*- coding: utf-8 -*-
 from RPi import GPIO
 import time
+import math
+
 GPIO.VERBOSE = True
+GPIO.setmode(GPIO.BCM)  # set up GPIO using BCM numbering
 
 class Magnet:
     '''Klasse som styrer elektromagneter. Tar inn en liste med porter som magnetene er koblet inn i, Magnet([port1, port2])'''
-    GPIO.setmode(GPIO.BCM)  # set up GPIO using BCM numbering
 
     def __init__(self, ports):
-        """Setter opp portnummerering til magneter og gjør disse til OUTPUT med PULL-DOWN-resistor"""
+        """Setter opp portnummerering til magneter og gjor disse til OUTPUT"""
 
         self.ports = ports
         for i in range(len(self.ports)):
-            GPIO.setup(self.ports[i],GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
+            GPIO.setup(self.ports[i],GPIO.OUT)
 
     def Drop(self):
-        """Objekt som skrur av og på magnetene"""
+        """Objekt som skrur av og paa magnetene"""
         self.Off()
         time.sleep(0.5)
         self.On()
 
     def On(self):
-        """Objekt som skrur på alle magneter"""
+        """Objekt som skrur paa alle magneter"""
         for i in range(len(self.ports)):
             GPIO.output(self.ports[i], True)
 
@@ -32,7 +34,7 @@ class Magnet:
             GPIO.output(self.ports[i], False)
 
 class Solution:
-    """Klasse som sjekker om oppgaven er utført korrekt. Tar inn tre lister med porter, porter til resistor og porter til kablene (self.NB (North Bridge) og self.SB (South Bridge))"""
+    """Klasse som sjekker om oppgaven er utfort korrekt. Tar inn tre lister med porter, porter til resistor og porter til kablene (self.NB (North Bridge) og self.SB (South Bridge))"""
 
     def __init__(self, resistorinputs, NBInputs, SBInputs, resistorvalue, bateryvoltage):
         self.resistors = resistorinputs
@@ -42,7 +44,7 @@ class Solution:
         self.voltage = bateryvoltage
 
         for i in range(len(self.SB)):
-            GPIO.setup(self.SB[i], GPIO.OUT, pull_up_down = GPIO.PUD_DOWN)
+            GPIO.setup(self.SB[i], GPIO.OUT)
 
         for i in range(len(self.NB)):
             GPIO.setup(self.NB[i], GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
@@ -50,8 +52,8 @@ class Solution:
         for i in range(len(self.resistors)):
             GPIO.setup(self.resistors[i], GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
-    def Exist(self):
-        """Objekt som sjekker om det er utført en løsning til problemet/oppgaven. Returnerer True hvis ja, False ellers"""
+    def ExistNoFilter(self):
+        """Objekt som sjekker om det er utfort en losning til problemet/oppgaven. Returnerer True hvis ja, False ellers"""
         paths = []
 
         for i in range(len(self.SB)):
@@ -59,54 +61,55 @@ class Solution:
 
             for j in range(len(self.NB)):
                 if GPIO.input(self.NB[j]):
-                    paths.append(self.NB[i])
+                    paths.append(self.NB[j])
 
             GPIO.output(self.SB[i], False)
 
-        if len(paths) == len(self.SB)**len(self.NB):
+        if len(paths) == len(self.SB):
             return True
         else:
             return False
 
     def Find(self):
-        """Objekt som sjekker om den utførte løsingen er korrekt. Returnerer True hvis ja, False ellers"""
+        """Objekt som sjekker om den utforte losingen er korrekt. Returnerer """
 
         resistors = self.FindResistors()
+        Voltage_2 = math.fsum(resistors[1])
+        Voltage_2 /=math.fsum(resistors[0])+math.fsum(resistors[1])
+        Voltage_2 *= self.voltage
 
-        return (resistors[1]/(resistors[0] + resistors[1]))*self.voltage
+        return Voltage_2
 
     def FindResistors(self):
-        """Objekt som sjekker hvilke motstander som er koblet inn"""
-        resistors = []
+        """Objekt som sjekker hvilke motstander som er koblet inn og returnerer med en liste"""
+        resistors = [[],[]]
         for i in range (len(self.SB)):
             GPIO.output(self.SB[i], True)
 
             for j in range (len(self.resistors)):
                 if GPIO.input(self.resistors[j]):
-                    resistors.append(self.value[j])
+                    resistors[i].append(self.value[j])
             GPIO.output(self.SB[i], False)
 
         return resistors
 
+    def FindResistorNumbers(self):
+        resistors = []
+        for i in range(len(self.SB)):
+            GPIO.output(self.SB[i], True)
 
+            for j in range(len(self.resistors)):
+                if GPIO.input(self.resistors[j]):
+                    resistors.append(j)
+            GPIO.output(self.SB[i], False)
+        return resistors
 
-#########TESTKODE#########
+    def Exist(self):
+        """Utforer self.ExistNoFilter 10 ganger. Returnerer True hvis alle iterasjonene faar True fra self.NoFilter, False ellers"""
+        for i in range(10):
 
-
-MagneterTest1 = Magnet([1])
-Løsning = Solution([1, 2], [3], [6, 5], [1000, 2000], 12)
-print(MagneterTest1.__doc__)
-print(Løsning.__doc__)
-
-MagneterTest1.On()
-MagneterTest1.Drop()
-MagneterTest1.Off()
-
-print("Run Løsning.Is()")
-print(Løsning.Exist())
-print(Løsning.Find())
-
-
-
+            if not(self.ExistNoFilter()):
+                return False
+        return True
 
 
